e01ffd8d74d99875e6cfd54ca37e2ab85dcfc876
Kevin <kevin.hanselman@gmail.com>
2014-05-07 15:59:23 -0400

    missed some changes to color change script

--- change-png-color.sh
+++ change-png-color.sh
@@ -4,8 +4,6 @@ prog=$(basename $0)
 
 error_out() { echo "$prog: $1" 1>&2; exit 1; }
 
-color='#DCDCDC'
-
 [ -d $1 ] || error_out "First argument must be an existing directory"
 
 [[ "$2" =~ [0-9A-Fa-f]{6} ]] || error_out "Second argument must be a hex number formatted as 'RRGGBB'"
@@ -14,6 +12,6 @@ pngs=$(find $1 -type f -iname '*.png')
 
 for p in $pngs; do
     echo "$p"
-    mogrify -fill "$color" -colorize 100% $p
+    mogrify -fill "#$2" -colorize 100% $p
 done
 
